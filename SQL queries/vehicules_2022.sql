4775b274c19d0211c126bc507801a9b3bd2dd54e
NoChip247 <156658200+NoChip247@users.noreply.github.com>
2024-04-05 14:37:00 +0200

    Update vehicules_2022.sql

--- SQL queries/vehicules_2022.sql
+++ SQL queries/vehicules_2022.sql
@@ -1,6 +1,6 @@
-# CLEANING
+-- CLEANING
 
-# La colonne id_vehicule a des espaces vides qui sont en fait des espaces insécables, et qui empêchent de CAST les STRING en INT64. Il faut d’abord enlever les espaces insécables, puis passer en INT64 la colonnes id_vehicule qui est une STRING.
+-- La colonne id_vehicule a des espaces vides qui sont en fait des espaces insécables, et qui empêchent de CAST les STRING en INT64. Il faut d’abord enlever les espaces insécables, puis passer en INT64 la colonnes id_vehicule qui est une STRING.
 SELECT
   Num_Acc,
   CAST(TRIM(REPLACE(id_vehicule, '\u00A0', '')) AS INT64) AS id_vehicule,
@@ -15,3 +15,17 @@ SELECT
   occutc
 FROM
   `avian-slice-411310.securite_routiere.vehicules_2022`
+
+-- PRIMARY KEY
+
+SELECT
+  Num_Acc,
+  COUNT(*) AS nb
+FROM
+  `avian-slice-411310.securite_routiere.vehicules_clean`
+GROUP BY
+  Num_Acc
+HAVING
+  nb > 2
+ORDER BY
+  nb desc
