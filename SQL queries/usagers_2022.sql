d08ae6ce66492ce8c2a54036f23fe529b8d4e36e
NoChip247 <156658200+NoChip247@users.noreply.github.com>
2024-04-05 13:27:21 +0200

    Create usagers_2022.sql
    Queries SQL concernant la table usagers_2022

---
+++ SQL queries/usagers_2022.sql
@@ -0,0 +1,21 @@
+# La colonne id_usager et id_vehicule ont des espaces vides qui sont en fait des espaces insécables, et qui empêchent de CAST les STRING en INT64. Il faut d’abord enlever les espaces insécables, puis passer en INT64 les colonnes id_usager et id_vehicule qui sont en STRING
+
+SELECT
+  Num_Acc,
+  CAST(TRIM(REPLACE(id_usager, '\u00A0', '')) AS INT64) AS id_usager,
+  CAST(TRIM(REPLACE(id_vehicule, '\u00A0', '')) AS INT64) AS id_vehicule,
+  num_veh,
+  place,
+  catu,
+  grav,
+  sexe,
+  an_nais,
+  trajet,
+  secu1,
+  secu2,
+  secu3,
+  locp,
+  actp,
+  etatp
+FROM
+  `avian-slice-411310.securite_routiere.usagers_2022`
