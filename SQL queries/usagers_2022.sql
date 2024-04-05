3a5c6575e139f08304a768b9263e49041df3e85e
NoChip247 <156658200+NoChip247@users.noreply.github.com>
2024-04-05 14:34:07 +0200

    Update usagers_2022.sql

--- SQL queries/usagers_2022.sql
+++ SQL queries/usagers_2022.sql
@@ -1,5 +1,6 @@
-# La colonne id_usager et id_vehicule ont des espaces vides qui sont en fait des espaces insécables, et qui empêchent de CAST les STRING en INT64. Il faut d’abord enlever les espaces insécables, puis passer en INT64 les colonnes id_usager et id_vehicule qui sont en STRING
+-- CLEANING
 
+-- La colonne id_usager et id_vehicule ont des espaces vides qui sont en fait des espaces insécables, et qui empêchent de CAST les STRING en INT64. Il faut d’abord enlever les espaces insécables, puis passer en INT64 les colonnes id_usager et id_vehicule qui sont en STRING
 SELECT
   Num_Acc,
   CAST(TRIM(REPLACE(id_usager, '\u00A0', '')) AS INT64) AS id_usager,
@@ -19,3 +20,17 @@ SELECT
   etatp
 FROM
   `avian-slice-411310.securite_routiere.usagers_2022`
+
+-- PRIMARY KEY
+
+SELECT
+  Num_Acc,
+  COUNT(*) AS nb
+FROM
+  `avian-slice-411310.securite_routiere.usagers_2022_clean`
+GROUP BY
+  Num_Acc
+HAVING
+  nb > 2
+ORDER BY
+  nb DESC
