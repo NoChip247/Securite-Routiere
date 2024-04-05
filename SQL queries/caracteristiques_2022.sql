44c746cd500b1ac7d5f9762ead36084048cbb718
NoChip247 <156658200+NoChip247@users.noreply.github.com>
2024-04-05 14:17:25 +0200

    Update caracteristiques_2022.sql

--- SQL queries/caracteristiques_2022.sql
+++ SQL queries/caracteristiques_2022.sql
@@ -1,9 +1,9 @@
-# Lors de l’import initial du .csv dans BigQuery, les latitudes et longitudes du .csv était des integers FR avec des virgules (ex : 1,5485699), mais BigQuery étant ENG, a supprimé les virgules, ainsi les latitudes et longitudes n’était alors plus utilisables (ex : 15485699).
-# Il a fallu réimporter le .csv en modifiant manuellement le schéma de la table pour indiquer à BigQuery que les colonnes lat et long était des STRING afin de pouvoir conserver les virgules.
+-- Lors de l’import initial du .csv dans BigQuery, les latitudes et longitudes du .csv était des integers FR avec des virgules (ex : 1,5485699), mais BigQuery étant ENG, a supprimé les virgules, ainsi les latitudes et longitudes n’était alors plus utilisables (ex : 15485699).
+-- Il a fallu réimporter le .csv en modifiant manuellement le schéma de la table pour indiquer à BigQuery que les colonnes lat et long était des STRING afin de pouvoir conserver les virgules.
 
-# CLEANING
+-- CLEANING
 
-# Transformer la colonne hrmn de string > time
+-- Transformer la colonne hrmn de string > time
 SELECT
     Accident_Id,
   jour,
@@ -24,6 +24,20 @@ SELECT
   long
 FROM
   `avian-slice-411310.securite_routiere.caracteristiques_2022_lat_long`
-# PARSE_TIME('%H:%M', hrmn) AS heures_minutes : on transforme la string de hrmn en format heures:minutes
-# EXTRACT(HOUR FROM PARSE_TIME('%H:%M', hrmn)) AS heures : on extrait l’heure
-# EXTRACT(MINUTE FROM PARSE_TIME('%H:%M', hrmn)) AS minutes : on extrait les minutes
+-- PARSE_TIME('%H:%M', hrmn) AS heures_minutes : on transforme la string de hrmn en format heures:minutes
+-- EXTRACT(HOUR FROM PARSE_TIME('%H:%M', hrmn)) AS heures : on extrait l’heure
+-- EXTRACT(MINUTE FROM PARSE_TIME('%H:%M', hrmn)) AS minutes : on extrait les minutes
+
+-- PRIMARY KEY
+
+SELECT
+  Accident_Id, 
+  COUNT(*) AS nb 
+FROM
+  `avian-slice-411310.securite_routiere.caracteristiques_clean`
+GROUP BY
+  Accident_Id 
+HAVING
+  nb > 2
+ORDER BY
+  nb desc 
