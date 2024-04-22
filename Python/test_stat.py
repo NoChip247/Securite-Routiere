a15f61043c22c9a5ad384024aaa0ca28b442fa02
NoChip247 <156658200+NoChip247@users.noreply.github.com>
2024-04-22 11:17:28 +0200

    Update test_stat.py

--- Python/test_stat.py
+++ Python/test_stat.py
@@ -1,4 +1,6 @@
-# On execute un Z-test pour obtenir la p_value et déterminer si notre hypothèse "il y a plus d'accident lorsqu'il pleut" est vrai ou non.
+# On execute un Z-test pour obtenir la p_value et répondre à la question : y a-t-il plus d'accident lorsqu'il pleut ? 
+# Hypothèse 0​ : Il n'y a pas de différence significative du nombre d'accidents sur la route entre les jours de pluie et les jours sans pluie.
+# Hypothèse 1​ : Il y a une différence significative du nombre d'accidents sur la route entre les jours de pluie et les jours sans pluie.
 
 import numpy as np
 from statsmodels.stats.weightstats import ztest
@@ -10,4 +12,7 @@ df_atm_acc
 # On lance le Z-test.
 z_score, p_value = ztest(df_atm_acc['atm'], df_atm_acc['Accident_Id'])
 print(f'p-value : {p_value}')
-# Résulat : p-value : 0.21328924662078408. On a un p_value supérieur à 5 % (ici 21 %) donc on rejète notre hypothèse "il y a plus d'accident lorsqu'il pleut" car on a 21 % de chance de se tromper (on accèpte 5 %).
+
+# Résulat : p-value : 0.21328924662078408.
+# Nous acceptons l'Hypothèse 0​ : Il n'y a pas de différence significative du nombre d'accidents sur la route entre les jours de pluie et les jours sans pluie.
+# Une p-value faible (inférieure au niveau de risque de 5% ou 0,05) indique que les données fournissent des preuves suffisantes pour rejeter l'hypothèse nulle, tandis qu'une p-value élevée (supérieure inférieure au niveau de risque de 5% ou 0,05) indique qu'il n'y a pas suffisamment de preuves pour rejeter l'hypothèse nulle.
